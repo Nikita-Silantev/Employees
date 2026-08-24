ad2cb53f3f5356934c52d285ebf1bc3c523b35e6
Nikita-Silantev <silantev.na2007@icloud.com>
2026-08-24 16:58:01 +0300

    feat(Server): сделал CRUD функции для отделов

--- Server/Repositories/EmployeeRepository.cs
+++ Server/Repositories/EmployeeRepository.cs
@@ -5,26 +5,37 @@ namespace Server.Repositories;
 
 public class EmployeeRepository
 {
-    private NpgsqlConnection CreateCOnnection()
+    /// <summary>
+    /// Создать подключение
+    /// </summary>
+    /// <returns></returns>
+    private NpgsqlConnection CreateConnection()
     {
         var builder = new NpgsqlConnectionStringBuilder
         {
             Host = "localhost",
             Port = 5432,
-            Database = "Employees",
+            Database = "employees",
             Username = "postgres",
-            Password = "postgres",
+            Password = "admin",
         };
 
         string connectionString = builder.ConnectionString;
         return new NpgsqlConnection(connectionString);
     }
 
+    #region отделы
+
+    /// <summary>
+    /// создать отдел
+    /// </summary>
+    /// <param name="department"></param>
+    /// <returns></returns>
     public async Task<Department?> CreateDepartment(Department department)
     {
         try
         {
-            await using var connection = CreateCOnnection();
+            await using var connection = CreateConnection();
             await connection.OpenAsync();
 
             const string sql = """
@@ -57,4 +68,96 @@ public class EmployeeRepository
             return null;
         }
     }
+
+    /// <summary>
+    /// выдать все отделы
+    /// </summary>
+    /// <returns></returns>
+    public async Task<List<Department?>> GetAllDepartment()
+    {
+        try
+        {
+            await using var connection = CreateConnection();
+            await connection.OpenAsync();
+
+            const string sql = "SELECT * FROM department";
+
+            await using var command = new NpgsqlCommand(sql, connection);
+            await using var reader = await command.ExecuteReaderAsync();
+
+            var departments = new List<Department>();
+            while (await reader.ReadAsync())
+            {
+                departments.Add(new Department
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("id")),
+                    Name = reader.GetString(reader.GetOrdinal("name"))
+                });
+            }
+
+            return departments;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// обновить отдел по id 
+    /// </summary>
+    /// <param name="department"></param>
+    /// <returns></returns>
+    public async Task<Department> UpdateDepartment(Department department)
+    {
+        await using var connection = CreateConnection();
+
+        await connection.OpenAsync();
+
+        const string sql = "update department set name = @name where id = @id returning id, name;";
+        await using var command = new NpgsqlCommand(sql, connection);
+
+        command.Parameters.AddWithValue("id", department.Id);
+        command.Parameters.AddWithValue("name", department.Name);
+
+        await using var rider = await command.ExecuteReaderAsync();
+        await rider.ReadAsync();
+        var new_department = new Department();
+
+        new_department.Id = rider.GetInt32(rider.GetOrdinal("id"));
+        new_department.Name = rider.GetString(rider.GetOrdinal("name"));
+
+        return new_department;
+    }
+
+    /// <summary>
+    /// удалить отдел по id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public async Task<string> DeleteDepartment(int id)
+    {
+        try
+        {
+            await using var connection = CreateConnection();
+            await connection.OpenAsync();
+
+            const string sql = "delete from department where id = @id;";
+
+            await using var command = new NpgsqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("id", id);
+
+            await command.ExecuteNonQueryAsync();
+            return "OK";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    #endregion
 }
--- Server/Services/EmployeeService.cs
+++ Server/Services/EmployeeService.cs
@@ -7,6 +7,14 @@ namespace Server.Services;
 
 public class EmployeeService : Employees.EmployeesBase
 {
+    #region отделы
+
+    /// <summary>
+    /// Создание отдела
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
     public override async Task<ResponceDepartment> CreateDepartment(RequestData request, ServerCallContext context)
     {
         var responce = new ResponceDepartment();
@@ -32,4 +40,77 @@ public class EmployeeService : Employees.EmployeesBase
 
         return responce;
     }
+
+    /// <summary>
+    /// выдать все отделы
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override async Task<AllDepartment> GetAllDepartments(EmptyRequest request, ServerCallContext context)
+    {
+        var response = new AllDepartment();
+        var rep = new EmployeeRepository();
+        var departments = await rep.GetAllDepartment();
+
+        foreach (Department d in departments)
+        {
+            response.Departments.Add(new DepartmentItem
+            {
+                Id = d.Id,
+                Name = d.Name
+            });
+        }
+
+        return response;
+    }
+
+    /// <summary>
+    /// обновить отдел по id 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override async Task<UpdatetDepartment> UpdateDepartment(ForUpdateDataDepartment request,
+        ServerCallContext context)
+    {
+        var response = new UpdatetDepartment();
+        var depatrment = new Department();
+        depatrment.Id = request.Id;
+        depatrment.Name = request.Name;
+
+        var repos = new EmployeeRepository();
+        var updated_department = await repos.UpdateDepartment(depatrment);
+
+        response.Id = updated_department.Id;
+        response.Name = updated_department.Name;
+
+        return response;
+    }
+
+    /// <summary>
+    /// удалить отдел по Id
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override async Task<StatusDelete> DeleteDepartment(ForDeleteDepartment request, ServerCallContext context)
+    {
+        var response = new StatusDelete();
+        int id = request.Id;
+        var repos = new EmployeeRepository();
+        var stat = await repos.DeleteDepartment(id);
+        if (stat == "OK")
+        {
+            response.Status = "Succsessful Delete Department";
+        }
+        else
+        {
+            response.Status = "Failed Delete Department";
+        }
+
+        return response;
+    }
+
+    #endregion
 }
