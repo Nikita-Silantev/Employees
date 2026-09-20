d9619d97c4a1c52df3e61e0f4b55b557c17a40a8
Никита <@DESKTOP-06ITJFB>
2026-09-20 23:43:20 +0300

    fest(Client): реализация crud для первой вкладки отделов, как todo поставил реализацию блокировки создания пока пустое поле ввода

--- ClientEmp/Services/ServiceGrpc.cs
+++ ClientEmp/Services/ServiceGrpc.cs
@@ -14,9 +14,13 @@ public class ServiceGrpc
 
     public ServiceGrpc(Employees.EmployeesClient client)
     {
-        _client =  client;
+        _client = client;
     }
 
+    /// <summary>
+    /// запрос на все отделы
+    /// </summary>
+    /// <returns></returns>
     public async Task<List<Department>> GetAllDepartment()
     {
         var responce = await _client.GetAllDepartmentsAsync(new EmptyRequest());
@@ -29,20 +33,70 @@ public class ServiceGrpc
                 Name = i.Name
             });
         }
+
         return allDepartments;
     }
 
+    /// <summary>
+    /// запрос на создание отдела
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
     public async Task<Department> CreateDepartment(string name)
     {
         try
         {
             var request = new RequestData();
             request.Name = name;
-            var responce = _client.CreateDepartment(request);
+            var responce = await _client.CreateDepartmentAsync(request);
             var createdDepartment = new Department();
             createdDepartment.Id = responce.Id;
             createdDepartment.Name = responce.Name;
             return createdDepartment;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// запрос на удаление отдела
+    /// </summary>
+    /// <param name="department"></param>
+    /// <returns></returns>
+    public async Task<StatusDelete> DeleteDepartment(Department department)
+    {
+        try
+        {
+            var responce = new StatusDelete();
+            var request = new ForDeleteDepartment();
+            request.Id = department.Id;
+            responce = await _client.DeleteDepartmentAsync(request);
+            return responce;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// запрос на обновление одела
+    /// </summary>
+    /// <param name="department"></param>
+    /// <returns></returns>
+    public async Task<UpdatetDepartment> UpdateDepartment(Department department)
+    {
+        try
+        {
+            var request = new ForUpdateDataDepartment();
+            request.Id = department.Id;
+            request.Name = department.Name;
+            UpdatetDepartment responce = await _client.UpdateDepartmentAsync(request);
+            return responce;
         }
         catch (Exception e)
         {
